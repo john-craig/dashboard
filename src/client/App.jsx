ab465117be5cc39fdbecada449bb3a0d9407bb5a
john-craig <john.craig1@marist.edu>
2020-10-24 22:09:16 -0400

    Update

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,7 +1,20 @@
 import { h, Component } from 'preact';
+import {gapi} from 'gapi';
 import Dashboard from './dashboard.jsx'
 
 class App extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            
+        }
+
+        const key = "AIzaSyCKH7DZlch5nq6An7l-ZpgBBtmqoWmOYnI"
+    }
+
+
     render() {
         return <Dashboard/>
     }
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const path = require("path");
+const webpack = require('webpack');
+const dotenv = require('dotenv').config( {
+  path: path.join(__dirname, '.env')
+} );
+
 module.exports = {
     mode: 'production',
     entry: {
@@ -38,10 +44,25 @@ module.exports = {
               loader: 'file-loader',
             },
           ],
+        },
+        {
+          test: /\.coffee$/,
+          loader: 'coffee-loader',
+          options: {
+            bare: false,
+            transpile: {
+              presets: ['@babel/env'],
+            },
+          },
         }
       ]
     },
     resolve: {
       extensions: ['.js', '.jsx']
-    }
+    },
+    plugins: [
+      new webpack.DefinePlugin( {
+        "process.env": dotenv.parsed
+      } ),
+    ]
   };
