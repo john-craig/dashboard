6206398cb753def882c2e79265cba7b7fe8cbb4e
john-craig <john.craig1@marist.edu>
2020-11-08 10:46:15 -0500

    Tweaked backend js

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,9 +1,58 @@
-// Listening to key press events
-chrome.runtime.onInstalled.addListener(function() {
-    // var script = document.createElement('script');
-    // script.onload=this.handleClientLoad;
-    // script.src="https://apis.google.com/js/api.js";
-    // document.body.appendChild(script);
+gapi = require('gapi-client');
 
+//Wait for page load
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("DOM loaded.")
 
+    //Wait for user to open a tab
+    chrome.tabs.onCreated.addListener(function(tab){
+        console.log("Tab opened.")
+
+        //Check to see if it is the newtab
+        if(tab.pendingUrl == "chrome://newtab/"){
+            console.log("Dashboard opened.")
+
+            // chrome.identity.getAuthToken({ interactive: true }, function (token) {
+            //     console.log(token);
+            // })
+
+            var script = document.createElement('script');
+            script.onload=handleClientLoad;
+            script.src="https://apis.google.com/js/client.js?onload=callbackFunction";
+            document.body.appendChild(script);
+        }
+    })
 })
+
+function handleClientLoad() {
+    console.log("Started client load...")
+
+    gapi.load(
+        'client:auth2', 
+        this.initClient
+    );
+}
+
+function initClient() { 
+    console.log("Started client initialization...")
+    const key = process.env.KEY
+    const clientId = process.env.CLIENTID
+    const scopes = 'profile';
+
+    gapi.client.init(
+        {
+            apiKey: key,
+            clientId: clientId,
+            scope: scopes,
+            discoveryDocs: ["https://people.googleapis.com/$discovery/rest?version=v1"]
+        },
+        function(err){
+            console.log(err);
+        }
+    ).then(
+        function () {
+            // do stuff with loaded APIs
+            console.log("Connection successful");
+        }
+    )
+}
