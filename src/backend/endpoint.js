9ace0f75c9e25be30b66ecf8d2512c11d2e3647c
john-craig <john.craig1@marist.edu>
2020-11-13 18:48:32 -0500

    Successfully queried google

--- src/backend/endpoint.js
+++ src/backend/endpoint.js
@@ -1,23 +1,28 @@
 const requestor = require('./request.js')
 
 export async function handleRequest(request, token){
-    const parameters = determineParameters(request.target)
+    const parameters = determineParameters(request)
 
     console.log("Sending a request.")
     return requestor.sendRequest(parameters['url'], parameters['type'], token, request.body)
 }
 
-function determineParameters(target){
+function determineParameters(request){
     var parameters = {
         'type': undefined,
         'url': undefined
     }
 
-    switch(target){
+    switch(request){
         case "driveFiles":
             parameters.type = "GET";
             parameters.url = "https://www.googleapis.com/drive/v2/files"
             break;
+
+        //Get all the files of a root directory
+        case "driveRoot":
+            parameters.type = "GET";
+            parameters.url = "https://www.googleapis.com/drive/v2/files/root/children"
     }
 
     return parameters
