2e72b18a6f4a2cd36a0e2eed399c9e1b2b9ffcdb
john-craig <john.craig1@marist.edu>
2020-11-14 10:57:17 -0500

    Implemented route for getting the current week log

--- src/backend/endpoint.js
+++ src/backend/endpoint.js
@@ -1,38 +1,24 @@
 const requestor = require('./request.js')
 
 export async function handleRequest(request, token){
-    const parameters = determineParameters(request)
-
-    console.log("Sending a request.")
-    console.log(parameters['url'])
-    return requestor.sendRequest(parameters['url'], parameters['type'], token, parameters['body'])
-}
-
-function determineParameters(request){
-    var parameters = {
-        'type': undefined,
-        'url': undefined,
-        'body': undefined
-    }
-
-    const queryURL = new URLSearchParams('q=p');
+    var response = undefined;
 
     switch(request.target){
-        case "driveFiles":
-            parameters.type = "GET";
-            parameters.url = "https://www.googleapis.com/drive/v2/files"
-            break;
-
-        //Get all the files of a root directory
         case "getFile":
-            queryURL.set('q', "name='" + request.argument + "'");
-            console.log(queryURL.toString())
-
-            //?q=name+%3d+%27" + request.argument + "%27"
-
-            parameters.type = "GET";
-            parameters.url = "https://www.googleapis.com/drive/v3/files?" + queryURL.toString()
+            response = getFile(request, token)
     }
 
-    return parameters
+    console.log("Sending a request.")
+
+    return response;
+}
+
+function getFile(request, token){
+    const type = "GET";
+    const query = new URLSearchParams('q=p');
+    query.set('q', "name = '" + request.argument + "'")
+
+    const url = "https://www.googleapis.com/drive/v3/files?" + query.toString()
+
+    return requestor.sendRequest(url, type, token, undefined)
 }
